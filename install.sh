9662ee212245c339d1d8f3eea1790024d15f8b03
julien <julien@ekohe.com>
2014-12-28 14:10:40 +0800

    add shell and color scheme file

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+cd ~
+rm -rf .vim .vimrc
+curl https://raw.githubusercontent.com/lxxdn/vimrc/master/vimrc >> .vimrc
+mkdir -p .vim/colors
+curl https://https://raw.githubusercontent.com/lxxdn/vimrc/master/molokai.vim >> .vim/colors/molokai.vim
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+### My vim config
+
+```sh
+curl -L bit.ly/1Ac5ZEw|sh
+```
+
+
