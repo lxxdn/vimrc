a21dedf582af5003bae6f79803bfe7673071664d
julien <julien@ekohe.com>
2015-01-04 10:46:40 +0800

    modify shell script to make it install solarized by default

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ cd ~
 rm -rf .vim .vimrc
 curl https://raw.githubusercontent.com/lxxdn/vimrc/master/vimrc >> .vimrc
 mkdir -p .vim/colors
-curl https://raw.githubusercontent.com/lxxdn/vimrc/master/molokai.vim >> .vim/colors/molokai.vim
+curl https://raw.githubusercontent.com/lxxdn/vimrc/master/solarized.vim >> .vim/colors/solarized.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
