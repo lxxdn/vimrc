a15613e5321e0c208ddd0b3deb32c0e3279008bf
julien <julien@ekohe.com>
2015-01-04 21:29:31 +0800

    update install shell

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 cd ~
 rm -rf .vim .vimrc
-curl https://raw.githubusercontent.com/lxxdn/vimrc/master/vimrc >> .vimrc
 mkdir -p .vim/colors
-curl https://raw.githubusercontent.com/lxxdn/vimrc/master/solarized.vim >> .vim/colors/solarized.vim
-curl https://raw.githubusercontent.com/lxxdn/vimrc/master/molokai.vim >> .vim/colors/molokai.vim
+git clone  https://github.com/lxxdn/vimrc.git
+mv vimrc/solarized.vim .vim/colors/solarized.vim
+mv vimrc/molokai.vim .vim/colors/molokai.vim
+mv vimrc/vimrc ~/.vimrc
+rm -rf vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
