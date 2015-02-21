98f51a34614fe918bb2ddd5f7e3a6dba156b15a9
julien <julien@ekohe.com>
2015-02-21 11:02:27 +0800

    add new color scheme

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ mkdir -p .vim/colors
 git clone  https://github.com/lxxdn/vimrc.git
 mv vimrc/solarized.vim .vim/colors/solarized.vim
 mv vimrc/molokai.vim .vim/colors/molokai.vim
+mv vimrc/Tomorrow-Night-Eighties.vim .vim/colors/molokai.vim
+
 mv vimrc/vimrc ~/.vimrc
 rm -rf vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
