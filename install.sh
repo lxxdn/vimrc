bdf2a43a3b715d2a96575994b0948bef0f03d427
julien <julien@ekohe.com>
2015-04-20 14:50:57 +0800

    remove fonts library

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #for install airline font
 git clone https://github.com/powerline/fonts.git fonts
 ./fonts/install.sh
+rm -rf fonts
